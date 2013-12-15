b0ca1c81ec474780e4d76b33ea4df00406808216
martinstrasek <strasek@live.com>
2013-12-15 17:35:42 +0100

    [ADD] rails new [MOD] routing root

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DormitoryWeb::Application.config.secret_key_base = 'd35863b139ea0c26c3947442c42f6d283028c7b69252227f6504c05ebe110d225922d43231df84c3656da7fbd9a879344ac1c642305d728b4ea7f194c5efb5d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DormitoryWeb::Application.config.session_store :cookie_store, key: '_dormitory-web_session'
