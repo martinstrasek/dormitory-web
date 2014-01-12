c99ca7fc6dd911717cadc05411a170e2f44728ef
Michal Mitter <mittermichal@gmail.com>
2014-01-12 15:38:54 +0100

    [mod] user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 DormitoryWeb::Application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  devise_for :users
+  resources :users#, :only => :show
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
