2614a1a392ee949598e0d786f7e186103eff3b17
martinstrasek <strasek@live.com>
2014-01-23 15:39:23 +0100

    [ADD] generate scaffold article - basic of article

---
+++ app/views/articles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @article, :id, :title, :body, :user_id, :published, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140110170150) do
+ActiveRecord::Schema.define(version: 20140123142035) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
