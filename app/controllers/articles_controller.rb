8fb1941a4a5549a9579a600b33b54ff677bc1ab3
martinstrasek <strasek@live.com>
2014-01-29 21:25:39 +0100

    [ADD] Adding Bootstrap template and simple form and modifiing current content to work together with bootstrap.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = current_user.articles.new(article_params)
-    
+
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
