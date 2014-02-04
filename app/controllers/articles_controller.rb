dc3b422e2090969bb893f15cd857752f05c69d0b
martinstrasek <strasek@live.com>
2014-02-04 19:04:43 +0100

    [MOD] Adding category association to the articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -73,6 +73,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :body, :published)
+      params.require(:article).permit(:title, :body, :category_id, :published)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140130014610) do
+ActiveRecord::Schema.define(version: 20140204163759) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20140130014610) do
     t.boolean  "published"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
+  add_index "articles", ["category_id"], name: "index_articles_on_category_id"
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "authorizations", force: true do |t|
